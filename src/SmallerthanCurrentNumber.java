6deca223ada54d9122f5b29ab0735cfb945cfb4d
Divyansh <yadavdivyansh445@gmail.com>
2025-07-28 22:44:19 +0530

    assignment 5 questions

---
+++ src/SmallerthanCurrentNumber.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class SmallerthanCurrentNumber {
+    public static void main(String[] args) {
+        int[] nums = {8,1,2,2,3};
+        System.out.println(Arrays.toString(smallerNumber(nums)));
+    }
+    static int[] smallerNumber(int[] nums){
+        int[] ans = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            int count=0;
+            for (int j = 0; j < nums.length; j++) {
+                if(j!=i&&nums[j]<nums[i]){
+                    count++;
+
+
+                }
+
+            }
+            ans[i] =count;
+        }
+
+        return ans;
+
+    }
+}
