9210cceb4b8dec8a6ada4d8842d79c82d76f517b
Divyansh <yadavdivyansh445@gmail.com>
2025-10-13 18:58:01 +0530

    spiral matrix 2

---
+++ src/SpiralMatrix2.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+
+public class SpiralMatrix2 {
+    public static void main(String[] args) {
+        int[][]result = generateMatrix(3);
+        System.out.println(Arrays.deepToString(result));
+    }
+    public static  int[][] generateMatrix(int n){
+        int r1 =0,r2=n-1;
+        int c1=0,c2=n-1;
+        int[][] arr = new int[n][n];
+        int val = 1;
+        while(r1<=r2 && c1<=c2){
+            // moving left to right
+            for(int c=c1;c<=c2;c++){
+                arr[r1][c] = val++;
+            }
+            // moving down
+            for (int r=r1+1;r<=r2;r++){
+                arr[r][c2] = val++;
+            }
+            // move right to left
+            // move up
+            if(r1<r2&&c1<c2){
+                // move right to left
+                for (int c=c2-1;c>c1;c--){
+                    arr[r2][c]= val++;
+                }
+                // move up
+                for (int r=r2;r>r1;r--){
+                    arr[r][c1]=val++;
+                }
+            }
+            r1++;
+            r2--;
+            c1++;
+            c2--;
+        }
+        return arr;
+    }
+}
